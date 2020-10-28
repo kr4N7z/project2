d38a40f06a5e0c90711ab80997670f543c9635ea
kr4N7z <73358932+kr4N7z@users.noreply.github.com>
2020-10-28 14:01:39 -0500

    updated validation

--- Project2/src/main/java/com/revature/utility/Validation.java
+++ Project2/src/main/java/com/revature/utility/Validation.java
@@ -2,4 +2,21 @@ package com.revature.utility;
 
 public class Validation {
 
+	private static Validation validation;
+
+	private Validation() {
+
+	}
+
+	public static Validation getInstance() {
+		if (validation == null) {
+			validation = new Validation();
+		}
+		return validation;
+	}
+
+	public boolean validEmail(String email) {
+		String regex = "^[\\w-_\\.+]*[\\w-_\\.]\\@([\\w]+\\.)+[\\w]+[\\w]$";
+		return email.matches(regex);
+	}
 }
