10659e47576806c4641dc09a5347f6a8616ab1b4
kr4N7z <73358932+kr4N7z@users.noreply.github.com>
2020-11-04 13:52:02 -0600

    back end session storage

--- Project2/src/main/java/com/revature/controller/FriendshipController.java
+++ Project2/src/main/java/com/revature/controller/FriendshipController.java
@@ -6,31 +6,36 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.revature.models.Friendship;
+import com.revature.models.User;
 import com.revature.repository.FriendshipRepositoryImpl;
 
 @RestController(value = "friendshipController")
 @RequestMapping(path = "/friendship")
 @CrossOrigin(origins = "http://wheretheboysat.s3-website-us-east-1.amazonaws.com")
+@SessionAttributes("currentUser")
+
 public class FriendshipController {
 	
 	
 	@RequestMapping(value = "/insert", method = RequestMethod.POST)
-	public void insertFriendship(@RequestParam("receiver_id") int receiverId, @RequestParam("approved") boolean approved, HttpSession session) {
+	public void insertFriendship(@RequestParam("receiver_id") int receiverId, @RequestParam("approved") boolean approved, @ModelAttribute("currentUser") User userAttribute) {
 		FriendshipRepositoryImpl frimpl = new FriendshipRepositoryImpl();
-		Friendship fr = new Friendship((int) session.getAttribute("sender_id"), receiverId, approved);
+		Friendship fr = new Friendship(userAttribute.getUserID(), receiverId, approved);
 		frimpl.insertFriendship(fr);
 	}
 	
 	@RequestMapping(value = "/update", method = RequestMethod.POST)
-	public void update(@RequestParam("receiver_id") int receiverId, @RequestParam("approved") boolean approved, HttpSession session) {
+	public void update(@RequestParam("receiver_id") int receiverId, @RequestParam("approved") boolean approved, @ModelAttribute("currentUser") User userAttribute) {
 		FriendshipRepositoryImpl frimpl = new FriendshipRepositoryImpl();
-		Friendship fr = new Friendship((int) session.getAttribute("sender_id"), receiverId, approved);
+		Friendship fr = new Friendship(userAttribute.getUserID(), receiverId, approved);
 		frimpl.update(fr);
 	}
 	
@@ -43,25 +48,25 @@ public class FriendshipController {
 	}
 	
 	@RequestMapping(value = "/viewMyFriendships", method = RequestMethod.GET)
-	public List<Friendship> viewMyFriendships(HttpSession session) {
+	public List<Friendship> viewMyFriendships(@ModelAttribute("currentUser") User userAttribute) {
 		FriendshipRepositoryImpl frimpl = new FriendshipRepositoryImpl();
-		List<Friendship> friends = frimpl.viewMyFriendships((int) session.getAttribute("sender_id"));
+		List<Friendship> friends = frimpl.viewMyFriendships(userAttribute.getUserID());
 		
 		return friends; 
 	}
 	
 	@RequestMapping(value = "/viewFriendship", method = RequestMethod.GET)
-	public Friendship getFriendship(@RequestParam("receiver_id") int receiverId, HttpSession session) {
+	public Friendship getFriendship(@RequestParam("receiver_id") int receiverId, @ModelAttribute("currentUser") User userAttribute) {
 		FriendshipRepositoryImpl frimpl = new FriendshipRepositoryImpl();
-		Friendship fr = frimpl.getFriendship((int) session.getAttribute("sender_id"), receiverId);
+		Friendship fr = frimpl.getFriendship(userAttribute.getUserID(), receiverId);
 		
 		return fr;
 	}
 	
 	@RequestMapping(value = "/removeFriendship", method = RequestMethod.POST)
-	public void removeFriendship(@RequestParam("receiver_id") int receiverId, HttpSession session) {
+	public void removeFriendship(@RequestParam("receiver_id") int receiverId, @ModelAttribute("currentUser") User userAttribute) {
 		FriendshipRepositoryImpl frimpl = new FriendshipRepositoryImpl();
-		Friendship fr = frimpl.getFriendship((int) session.getAttribute("sender_id"), receiverId);
+		Friendship fr = frimpl.getFriendship(userAttribute.getUserID(), receiverId);
 		frimpl.removeFriendship(fr);
 	}
 }
--- Project2/src/main/java/com/revature/controller/UserController.java
+++ Project2/src/main/java/com/revature/controller/UserController.java
@@ -4,18 +4,25 @@ import java.util.Iterator;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.google.gson.Gson;
 import com.revature.models.User;
+import com.revature.repository.UserRepository;
+import com.revature.repository.UserRepositoryImpl;
 import com.revature.service.UserService;
+import com.revature.utility.Encryption;
 
 /*
  * Using a Spring controller we can easily define endpoints and mappings!
@@ -27,27 +34,62 @@ import com.revature.service.UserService;
  */
 @RestController(value = "userController")
 @RequestMapping(path = "/user")
-//@CrossOrigin(origins="http://localhost:4200")
-@CrossOrigin(origins = "http://wheretheboysat.s3-website-us-east-1.amazonaws.com")
+@CrossOrigin(origins="http://localhost:4200")
+@SessionAttributes("currentUser")
+//@CrossOrigin(origins = "http://wheretheboysat.s3-website-us-east-1.amazonaws.com")
 public class UserController {
 	UserService userService;
-
+	UserRepository userRepo;
+	PasswordEncoder enc;
+	
 	UserController(){
 		userService = new UserService();
+		userRepo = new UserRepositoryImpl();
+		enc = Encryption.getEncoder();
 	}
-
+	
 	@RequestMapping(value = "/register", method = RequestMethod.POST)
 	public void registertUser(@RequestBody User user) {
 		userService.register(user);
 	}
 
 	@RequestMapping(value="/login", method=RequestMethod.POST)
-	public User login(@RequestBody String body, HttpServletRequest req) {
+	public User login(@RequestBody String body, HttpServletRequest req, HttpServletResponse response,@ModelAttribute("currentUser") User userAttribute) {
 		Gson gson = new Gson();
 		User user = gson.fromJson(body, User.class);
+		
+		User userDb = userRepo.findOneByEmail(user.getEmail());
+		if (enc.matches(user.getPassword(), userDb.getPassword())) {
+			System.out.println("got a match trying to create a session");
+			//try {
+				//String remoteAddress = req.getRemoteAddr();
+				//String remoteAddress = req.getLocalAddr();
+				//InetAddress ipAddress = InetAddress.getByName(remoteAddress);
+				//GeoIpService geoIpService = new GeoIpService();
+				//GeoIp location = geoIpService.getLocation(ipAddress);
+				//user.setLastState(location.getState());
+				//user.setLatitude(Float.valueOf(location.getLatitude()));
+				//user.setLongitude(Float.valueOf(location.getLongitude()));
 
-		//return "user email "+ user.getEmail() + " user password " + user.getPassword();
-		return userService.login(user.getEmail(), user.getPassword(), req);
+			//} catch (UnknownHostException e) {
+				// TODO Auto-generated catch block
+			//	e.printStackTrace();
+			//}
+//			
+			
+			req.getSession().setAttribute("user_id", userDb.getUserID());
+			req.getSession().setAttribute("first_name", userDb.getFirstName());
+			req.getSession().setAttribute("last_name", userDb.getLastName());
+			userAttribute.setFirstName(userDb.getFirstName());
+			userAttribute.setLastName(userDb.getLastName());
+			userAttribute.setUserID(userDb.getUserID());
+
+			//String valueString =gson.toJson(user);
+			//Cookie createSession = new Cookie(req.getSession().getId(), valueString);
+			//createSession.setPath(req.getContextPath());
+			//response.addCookie(createSession);
+		}
+			return userDb;
 	}
 
 	@RequestMapping(value="/logout", method=RequestMethod.GET)
@@ -56,13 +98,19 @@ public class UserController {
 	}
 
 	@RequestMapping(value = "/myfriends", method = RequestMethod.GET)
-	public List<User> getFriends(HttpSession session) {
-		List<User> friends = userService.getFriends(Integer.valueOf(session.getAttribute("userID").toString()));
+	public List<User> getFriends(@ModelAttribute("currentUser") User userAttribute) {
+		System.out.println(userAttribute.getUserID());
+		List<User> friends = userService.getFriends(userAttribute.getUserID());
 		return friends;
 	}
 	@RequestMapping(value = "/allusers", method = RequestMethod.GET)
 	public List<User> getAllUsers(HttpSession session) {
 		List<User> users = userService.getAllUsers();
 		return users;
+	}
+	
+	@ModelAttribute("currentUser")
+	public User userAttributes() {
+		return new User();
 	}
 }
