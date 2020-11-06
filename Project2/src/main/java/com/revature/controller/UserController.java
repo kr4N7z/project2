f7e11273405e2bc9faf4c7f8cb6c13e4c680734a
kr4N7z <73358932+kr4N7z@users.noreply.github.com>
2020-11-05 20:36:13 -0600

    Revert "autowired implented"
    This reverts commit 8869a5cade7893c8a3ff3c978a42f6c78427e250.

--- Project2/src/main/java/com/revature/controller/UserController.java
+++ Project2/src/main/java/com/revature/controller/UserController.java
@@ -8,7 +8,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -18,7 +17,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.SessionAttributes;
-import org.springframework.web.bind.support.SessionStatus;
 
 import com.google.gson.Gson;
 import com.revature.models.User;
@@ -27,6 +25,7 @@ import com.revature.repository.UserRepositoryImpl;
 import com.revature.service.UserService;
 import com.revature.utility.BasicResponseWrapper;
 import com.revature.utility.Encryption;
+
 
 /*
  * Using a Spring controller we can easily define endpoints and mappings!
@@ -42,14 +41,14 @@ import com.revature.utility.Encryption;
 @SessionAttributes("currentUser")
 //@CrossOrigin(origins = "http://wheretheboysat.s3-website-us-east-1.amazonaws.com")
 public class UserController {
-	@Autowired
 	UserService userService;
-	@Autowired
 	UserRepository userRepo;
-	@Autowired
 	PasswordEncoder enc;
 
-	UserController() {
+	UserController(){
+		userService = new UserService();
+		userRepo = new UserRepositoryImpl();
+		enc = Encryption.getEncoder();
 	}
 
 	@RequestMapping(value = "/register", method = RequestMethod.POST)
@@ -58,29 +57,28 @@ public class UserController {
 		return userService.register(user);
 	}
 
-	@RequestMapping(value = "/login", method = RequestMethod.POST)
-	public User login(@RequestBody String body, HttpServletRequest req, HttpServletResponse response,
-			@ModelAttribute("currentUser") User userAttribute) {
+	@RequestMapping(value="/login", method=RequestMethod.POST)
+	public User login(@RequestBody String body, HttpServletRequest req, HttpServletResponse response,@ModelAttribute("currentUser") User userAttribute) {
 		Gson gson = new Gson();
 		User user = gson.fromJson(body, User.class);
 
 		User userDb = userRepo.findOneByEmail(user.getEmail());
 		if (enc.matches(user.getPassword(), userDb.getPassword())) {
 			System.out.println("got a match trying to create a session");
-			// try {
-			// String remoteAddress = req.getRemoteAddr();
-			// String remoteAddress = req.getLocalAddr();
-			// InetAddress ipAddress = InetAddress.getByName(remoteAddress);
-			// GeoIpService geoIpService = new GeoIpService();
-			// GeoIp location = geoIpService.getLocation(ipAddress);
-			// user.setLastState(location.getState());
-			// user.setLatitude(Float.valueOf(location.getLatitude()));
-			// user.setLongitude(Float.valueOf(location.getLongitude()));
+			//try {
+				//String remoteAddress = req.getRemoteAddr();
+				//String remoteAddress = req.getLocalAddr();
+				//InetAddress ipAddress = InetAddress.getByName(remoteAddress);
+				//GeoIpService geoIpService = new GeoIpService();
+				//GeoIp location = geoIpService.getLocation(ipAddress);
+				//user.setLastState(location.getState());
+				//user.setLatitude(Float.valueOf(location.getLatitude()));
+				//user.setLongitude(Float.valueOf(location.getLongitude()));
 
-			// } catch (UnknownHostException e) {
-			// TODO Auto-generated catch block
-			// e.printStackTrace();
-			// }
+			//} catch (UnknownHostException e) {
+				// TODO Auto-generated catch block
+			//	e.printStackTrace();
+			//}
 //
 
 			req.getSession().setAttribute("user_id", userDb.getUserID());
@@ -90,18 +88,17 @@ public class UserController {
 			userAttribute.setLastName(userDb.getLastName());
 			userAttribute.setUserID(userDb.getUserID());
 
-			// String valueString =gson.toJson(user);
-			// Cookie createSession = new Cookie(req.getSession().getId(), valueString);
-			// createSession.setPath(req.getContextPath());
-			// response.addCookie(createSession);
+			//String valueString =gson.toJson(user);
+			//Cookie createSession = new Cookie(req.getSession().getId(), valueString);
+			//createSession.setPath(req.getContextPath());
+			//response.addCookie(createSession);
 		}
-		return userDb;
+			return userDb;
 	}
 
-	@RequestMapping(value = "/logout", method = RequestMethod.GET)
-	public void logout(HttpServletRequest req, SessionStatus status) {
-		status.setComplete();
-		userService.logout(req);
+	@RequestMapping(value="/logout", method=RequestMethod.GET)
+	public void logout( HttpServletRequest req) {
+		 userService.logout( req);
 	}
 
 	@RequestMapping(value = "/myfriends", method = RequestMethod.GET)
@@ -111,39 +108,41 @@ public class UserController {
 //		List<User> friends = userService.getFriends(userId);
 
 		System.out.println("we entered the myfriends controller");
-		// if(session==null) {
-		// System.out.println("session is null!");
-		// }else {
-		// System.out.println("trying to print the attribute names: ");
-		// Iterator<String> iterator = session.getAttributeNames().asIterator();
-		// while(iterator.hasNext()) {
-		// System.out.println("iterator item: "+ iterator.next());
-		// }
-		// }
+		//if(session==null) {
+			//System.out.println("session is null!");
+		//}else {
+			//System.out.println("trying to print the attribute names: ");
+			//Iterator<String> iterator = session.getAttributeNames().asIterator();
+			//while(iterator.hasNext()) {
+			//	System.out.println("iterator item: "+ iterator.next());
+			//}
+		//}
 		List<User> friends = userService.getFriends(Integer.valueOf(userAttribute.getUserID()));
+
 
 		return friends;
 	}
-
+	
 	@RequestMapping(value = "/allusers", method = RequestMethod.GET)
 	public List<User> getAllUsers() {
 		List<User> users = userService.getAllUsers();
 		return users;
 	}
 
-	// @RequestMapping(value = "/testSession", method = RequestMethod.POST)
-	// public ArrayList<String> getAllUsers(HttpSession session) {
-	// ArrayList<String> res = new ArrayList<>();
-	// Iterator<String> iterator = session.getAttributeNames().asIterator();
-	// System.out.println("printing out the session attribute names");
-	// while(iterator.hasNext()) {
-	/// String current = iterator.next();
-	// res.add(current);
-	// System.out.println("iterator item: "+ current);
 
-	// }
-	// return res;
-	// }
+	//@RequestMapping(value = "/testSession", method = RequestMethod.POST)
+	//public ArrayList<String> getAllUsers(HttpSession session) {
+		//ArrayList<String> res = new ArrayList<>();
+		//Iterator<String> iterator = session.getAttributeNames().asIterator();
+		//System.out.println("printing out the session attribute names");
+		//while(iterator.hasNext()) {
+		///	String current = iterator.next();
+		//	res.add(current);
+		//	System.out.println("iterator item: "+ current);
+
+		//}
+		//return res;
+	//}
 
 	@ModelAttribute("currentUser")
 	public User userAttributes() {
