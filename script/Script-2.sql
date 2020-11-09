ddb7007d5d295e2b22353af8c96a98552f50fd19
TerranceDwan <terrance4869@gmail.com>
2020-11-08 20:12:34 -0500

    added dummy data script

---
+++ script/Script-2.sql
@@ -0,0 +1,273 @@
+insert into users values(
+	default,
+	'standard',
+	'donuts@email.com',
+	'$2a$10$iFQZDh2FalUNoNaVzFCIYucehz4KK4GU541JQ4j3Dx46Evg4/ZZWW',
+	'Homer',
+	'Simpson',
+	100,
+	100,
+	'Illinois',
+	'2016-06-22 19:10:25-07',
+	'2020-06-22 19:10:25-07'
+	);
+insert into users values(
+	default,
+	'standard',
+	'ninja@email.com',
+	'$2a$10$iFQZDh2FalUNoNaVzFCIYucehz4KK4GU541JQ4j3Dx46Evg4/ZZWW',
+	'Naruto',
+	'Uzumaki',
+	100,
+	100,
+	'Oregon',
+	'2016-06-22 19:10:25-07',
+	'2020-06-22 19:10:25-07'
+	);
+insert into users values(
+	default,
+	'standard',
+	'bombs@email.com',
+	'$2a$10$iFQZDh2FalUNoNaVzFCIYucehz4KK4GU541JQ4j3Dx46Evg4/ZZWW',
+	'Duke',
+	'Nukem',
+	100,
+	100,
+	'California',
+	'2016-06-22 19:10:25-07',
+	'2020-06-22 19:10:25-07'
+	);
+insert into users values(
+	default,
+	'standard',
+	'Ihatekyle@email.com',
+	'$2a$10$iFQZDh2FalUNoNaVzFCIYucehz4KK4GU541JQ4j3Dx46Evg4/ZZWW',
+	'Eric',
+	'Cartman',
+	100,
+	100,
+	'Colorado',
+	'2016-06-22 19:10:25-07',
+	'2020-06-22 19:10:25-07'
+	);
+insert into users values(
+	default,
+	'standard',
+	'bluehat@email.com',
+	'$2a$10$iFQZDh2FalUNoNaVzFCIYucehz4KK4GU541JQ4j3Dx46Evg4/ZZWW',
+	'Stan',
+	'Marsh',
+	100,
+	100,
+	'Colorado',
+	'2016-06-22 19:10:25-07',
+	'2020-06-22 19:10:25-07'
+	);
+insert into users values(
+	default,
+	'standard',
+	'gold@email.com',
+	'$2a$10$iFQZDh2FalUNoNaVzFCIYucehz4KK4GU541JQ4j3Dx46Evg4/ZZWW',
+	'Kyle',
+	'Broflovski',
+	100,
+	100,
+	'Colorado',
+	'2016-06-22 19:10:25-07',
+	'2020-06-22 19:10:25-07'
+	);
+insert into users values(
+	default,
+	'standard',
+	'muffled@email.com',
+	'$2a$10$iFQZDh2FalUNoNaVzFCIYucehz4KK4GU541JQ4j3Dx46Evg4/ZZWW',
+	'Kenny',
+	'McCormick',
+	100,
+	100,
+	'Colorado',
+	'2016-06-22 19:10:25-07',
+	'2020-06-22 19:10:25-07'
+	);
+insert into users values(
+	default,
+	'standard',
+	'karate@email.com',
+	'$2a$10$iFQZDh2FalUNoNaVzFCIYucehz4KK4GU541JQ4j3Dx46Evg4/ZZWW',
+	'Chuck',
+	'Norris',
+	100,
+	100,
+	'Texas',
+	'2016-06-22 19:10:25-07',
+	'2020-06-22 19:10:25-07'
+	);
+insert into users values(
+	default,
+	'standard',
+	'freshprince@email.com',
+	'$2a$10$iFQZDh2FalUNoNaVzFCIYucehz4KK4GU541JQ4j3Dx46Evg4/ZZWW',
+	'Will',
+	'Smith',
+	100,
+	100,
+	'Pennsylvania',
+	'2016-06-22 19:10:25-07',
+	'2020-06-22 19:10:25-07'
+	);
+insert into users values(
+	default,
+	'standard',
+	'bbq@email.com',
+	'$2a$10$iFQZDh2FalUNoNaVzFCIYucehz4KK4GU541JQ4j3Dx46Evg4/ZZWW',
+	'Hank',
+	'Hill',
+	100,
+	100,
+	'Texas',
+	'2016-06-22 19:10:25-07',
+	'2020-06-22 19:10:25-07'
+	);
+insert into users values(
+	default,
+	'standard',
+	'bobbyboy@email.com',
+	'$2a$10$iFQZDh2FalUNoNaVzFCIYucehz4KK4GU541JQ4j3Dx46Evg4/ZZWW',
+	'Bobby',
+	'Hill',
+	100,
+	100,
+	'Texas',
+	'2016-06-22 19:10:25-07',
+	'2020-06-22 19:10:25-07'
+	);
+insert into users values(
+	default,
+	'standard',
+	'nypd99@email.com',
+	'$2a$10$iFQZDh2FalUNoNaVzFCIYucehz4KK4GU541JQ4j3Dx46Evg4/ZZWW',
+	'Jake',
+	'Peralta',
+	100,
+	100,
+	'New York',
+	'2016-06-22 19:10:25-07',
+	'2020-06-22 19:10:25-07'
+	);
+insert into users values(
+	default,
+	'standard',
+	'yogurt@email.com',
+	'$2a$10$iFQZDh2FalUNoNaVzFCIYucehz4KK4GU541JQ4j3Dx46Evg4/ZZWW',
+	'Terry',
+	'Jeffords',
+	100,
+	100,
+	'New York',
+	'2016-06-22 19:10:25-07',
+	'2020-06-22 19:10:25-07'
+	);
+insert into users values(
+	default,
+	'standard',
+	'serious@email.com',
+	'$2a$10$iFQZDh2FalUNoNaVzFCIYucehz4KK4GU541JQ4j3Dx46Evg4/ZZWW',
+	'Raymond',
+	'Holt',
+	100,
+	100,
+	'New York',
+	'2016-06-22 19:10:25-07',
+	'2020-06-22 19:10:25-07'
+	);
+insert into users values(
+	default,
+	'standard',
+	'binders@email.com',
+	'$2a$10$iFQZDh2FalUNoNaVzFCIYucehz4KK4GU541JQ4j3Dx46Evg4/ZZWW',
+	'Amy',
+	'Santiago',
+	100,
+	100,
+	'New York',
+	'2016-06-22 19:10:25-07',
+	'2020-06-22 19:10:25-07'
+	);
+insert into users values(
+	default,
+	'standard',
+	'ballet@email.com',
+	'$2a$10$iFQZDh2FalUNoNaVzFCIYucehz4KK4GU541JQ4j3Dx46Evg4/ZZWW',
+	'Rosa',
+	'Diaz',
+	100,
+	100,
+	'New York',
+	'2016-06-22 19:10:25-07',
+	'2020-06-22 19:10:25-07'
+	);
+insert into users values(
+	default,
+	'standard',
+	'nicolaj@email.com',
+	'$2a$10$iFQZDh2FalUNoNaVzFCIYucehz4KK4GU541JQ4j3Dx46Evg4/ZZWW',
+	'Charles',
+	'Boyle',
+	100,
+	100,
+	'New York',
+	'2016-06-22 19:10:25-07',
+	'2020-06-22 19:10:25-07'
+	);
+insert into users values(
+	default,
+	'standard',
+	'jags@email.com',
+	'$2a$10$iFQZDh2FalUNoNaVzFCIYucehz4KK4GU541JQ4j3Dx46Evg4/ZZWW',
+	'Jason',
+	'Mendoza',
+	100,
+	100,
+	'Florida',
+	'2016-06-22 19:10:25-07',
+	'2020-06-22 19:10:25-07'
+	);
+insert into users values(
+	default,
+	'standard',
+	'antman@email.com',
+	'$2a$10$iFQZDh2FalUNoNaVzFCIYucehz4KK4GU541JQ4j3Dx46Evg4/ZZWW',
+	'Scott',
+	'Lang',
+	100,
+	100,
+	'New York',
+	'2016-06-22 19:10:25-07',
+	'2020-06-22 19:10:25-07'
+	);
+insert into users values(
+	default,
+	'standard',
+	'ohana@email.com',
+	'$2a$10$iFQZDh2FalUNoNaVzFCIYucehz4KK4GU541JQ4j3Dx46Evg4/ZZWW',
+	'Lilo',
+	'Pelekai',
+	100,
+	100,
+	'Hawaii',
+	'2016-06-22 19:10:25-07',
+	'2020-06-22 19:10:25-07'
+	);
+insert into users values(
+	default,
+	'standard',
+	'lilo@email.com',
+	'$2a$10$iFQZDh2FalUNoNaVzFCIYucehz4KK4GU541JQ4j3Dx46Evg4/ZZWW',
+	'Stitch',
+	'Pelekai',
+	100,
+	100,
+	'Hawaii',
+	'2016-06-22 19:10:25-07',
+	'2020-06-22 19:10:25-07'
+	);
